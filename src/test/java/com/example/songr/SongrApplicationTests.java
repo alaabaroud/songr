03c02ab23f9a298586fd81e7939c85bd22478039
ALAABAROUD <alaabaroud783@gmail.com>
2021-10-29 16:32:23 +0200

    lab11

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -8,6 +8,9 @@ class SongrApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+
+		AlbumModel test = new AlbumModel("test", "test", 10 , 20, "https://img.discogs.com/u3e2tWPxqlJjJ8ymLHYGl94y44w=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9920546-1488569067-1787.jpeg.jpg");
 	}
 
 }
